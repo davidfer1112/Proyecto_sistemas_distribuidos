e54f5c940c0e5cff9ae667b98ebf348192f3619f
davidfer <davidfernando1112@gmail.com>
2023-10-02 20:49:35 -0500

    Feature control de parametros  Sensor

--- src/main/java/org/distribuidos/Sensor/Sensor.java
+++ src/main/java/org/distribuidos/Sensor/Sensor.java
@@ -1,18 +1,29 @@
 package org.distribuidos.Sensor;
 
-import org.distribuidos.Mensaje.Mensaje;
 
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.Date;
+import org.zeromq.ZMQ;
 
 import org.zeromq.ZMQ;
 
 public class Sensor {
     public static void main(String[] args) {
+        // Verifica que se ingresen tres argumentos
+        if (args.length != 3) {
+            System.out.println("La forma correcta de los parámetros es: tipoSensor Tiempo archivoConfig");
+            return; // Sale del programa si no hay tres argumentos
+        }
+
         String sensorType = args[0];
-        int sendInterval = Integer.parseInt(args[1]);
+        int sendInterval;
         String configFileName = args[2];
+
+        // Verifica que el segundo argumento sea un entero válido
+        try {
+            sendInterval = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("El segundo argumento (Tiempo) debe ser un número entero válido.");
+            return; // Sale del programa si el segundo argumento no es un entero válido
+        }
 
         // Aquí deberías leer el archivo de configuración y obtener las probabilidades.
 
