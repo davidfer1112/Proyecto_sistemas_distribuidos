bdd7f5b7fa686a4a22e143d8e150fbb5c00b41d9
davidfer1112 <davidfernando1112@gmail.com>
2023-10-30 18:27:45 -0500

    Feature: reporte de  sitema de calidad

--- src/main/java/org/distribuidos/Monitor/Monitor.java
+++ src/main/java/org/distribuidos/Monitor/Monitor.java
@@ -1,12 +1,5 @@
 package org.distribuidos.Monitor;
 
-
-
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import org.zeromq.ZMQ;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,34 +18,56 @@ public class Monitor {
         String sensorType = args[0];
 
         try (ZMQ.Context context = ZMQ.context(1);
-             ZMQ.Socket subscriber = context.socket(ZMQ.SUB)) {
+             ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+             ZMQ.Socket systemQualityPublisher = context.socket(ZMQ.PUB)) {
 
             // Cambia la conexión al canal de suscripción para que sea la dirección IP de la computadora que ejecuta el programa 'Sensor'.
             subscriber.connect("tcp://192.168.0.12:5556");
             subscriber.subscribe(sensorType.getBytes());
 
-            // Abre el archivo para escribir
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("reporte.txt", true))) {
-                while (true) {
-                    // Recibe el mensaje de saludo del canal de suscripción
-                    String message = subscriber.recvStr();
-                    Date date = new Date();
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                    String dateString = dateFormat.format(date);
+            // Conexión al canal de publicación del Sistema de Calidad
+            systemQualityPublisher.bind("tcp://*:5557");
 
-                    System.out.println("["+dateString+"]"+"Mensaje: " + message);
+            while (true) {
+                // Recibe el mensaje del canal de suscripción
+                String message = subscriber.recvStr();
+                Date date = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                String dateString = dateFormat.format(date);
 
-                    message = "["+dateString+"]"+ message;
-                    // Escribe el mensaje en el archivo
+                System.out.println("[" + dateString + "] Mensaje: " + message);
+
+                message = "[" + dateString + "]" + message;
+
+                // Escribe el mensaje en el archivo
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("reporte.txt", true))) {
                     writer.write(message);
                     writer.newLine(); // Agrega una nueva línea para separar los mensajes
                     writer.flush(); // Asegura que los datos se escriban en el archivo
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+
+                // Verifica si el mensaje indica un error y envía al Sistema de Calidad
+                if (message.contains("Errores")) {
+                    // Envía mensaje al Sistema de Calidad
+                    String systemQualityMessage = "[" + dateString + "] Error en el monitor de " + sensorType +
+                            " con valor: " + parseErrorValue(message);
+                    systemQualityPublisher.send(systemQualityMessage);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String parseErrorValue(String errorMessage) {
+        // Realiza el análisis del mensaje de error para extraer el valor
+        String[] parts = errorMessage.split(" ");
+        if (parts.length >= 10) {
+            return parts[9];
+        } else {
+            return "Valor Desconocido";
         }
     }
 }
--- src/main/java/org/distribuidos/sitemaCalidad/SistemaCalidad.java
+++ src/main/java/org/distribuidos/sitemaCalidad/SistemaCalidad.java
@@ -1,4 +1,37 @@
 package org.distribuidos.sitemaCalidad;
 
+import org.zeromq.ZMQ;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SistemaCalidad {
+    public static void main(String[] args) {
+        try (ZMQ.Context context = ZMQ.context(1);
+             ZMQ.Socket monitorSubscriber = context.socket(ZMQ.SUB)) {
+
+            // Cambia la conexión al canal de publicación del Monitor
+            monitorSubscriber.connect("tcp://192.168.0.12:5557"); // Ajusta la dirección y el puerto según el Monitor
+            monitorSubscriber.subscribe("".getBytes());
+
+            while (true) {
+                // Recibe mensajes del Monitor
+                String monitorMessage = monitorSubscriber.recvStr();
+                Date date = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                String dateString = dateFormat.format(date);
+
+                // Procesa el mensaje recibido (puedes personalizar esto según tus necesidades)
+                processMonitorMessage(dateString, monitorMessage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processMonitorMessage(String dateString, String monitorMessage) {
+        // Analiza el mensaje del Monitor y realiza las acciones necesarias
+        System.out.println("[" + dateString + "] " + monitorMessage);
+        // Puedes agregar lógica adicional aquí, como enviar notificaciones, generar informes, etc.
+    }
 }
