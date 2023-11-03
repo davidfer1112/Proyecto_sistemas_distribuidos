c17e696eaa44c1124c4a0506d04da7bdd6a6150f
davidfer1112 <davidfernando1112@gmail.com>
2023-11-02 22:27:57 -0500

    Feature: tiempo caducado inicio de redireccion

--- src/main/java/org/distribuidos/healthCheck/HealthCheck.java
+++ src/main/java/org/distribuidos/healthCheck/HealthCheck.java
@@ -3,25 +3,48 @@ package org.distribuidos.healthCheck;
 import org.zeromq.ZMQ;
 
 public class HealthCheck {
+    private static final int TIMEOUT = 5000;  // Tiempo en milisegundos
+    private static final int POLL_TIMEOUT = 1000;  // Tiempo de espera para el evento de mensaje (en milisegundos)
+
     public static void main(String[] args) {
         try (ZMQ.Context context = ZMQ.context(1);
              ZMQ.Socket intervalSubscriber = context.socket(ZMQ.SUB)) {
 
-            intervalSubscriber.connect("tcp://*:5559");  // Ajusta el puerto según tus necesidades
+            intervalSubscriber.connect("tcp://*:5559");
             intervalSubscriber.subscribe("".getBytes());
 
+            long lastMessageTime = System.currentTimeMillis();
+
             while (true) {
-                // Recibe el valor de sendInterval del Monitor
-                String intervalMessage = intervalSubscriber.recvStr();
-                int sendInterval = Integer.parseInt(intervalMessage);
+                // Configura el conjunto de sockets para el evento de entrada
+                ZMQ.Poller poller = context.poller(1);
+                poller.register(intervalSubscriber, ZMQ.Poller.POLLIN);
 
-                // Imprime el valor por consola
-                System.out.println("Valor de sendInterval recibido por HealthCheck: " + sendInterval);
-
-                // Realiza otras acciones según tus necesidades
+                // Espera eventos durante el tiempo de espera del poller
+                if (poller.poll(POLL_TIMEOUT) > 0) {
+                    // Si hay un mensaje, lo procesa
+                    String intervalMessage = intervalSubscriber.recvStr();
+                    int sendInterval = Integer.parseInt(intervalMessage);
+                    System.out.println("Valor de sendInterval recibido por HealthCheck: " + sendInterval);
+                    lastMessageTime = System.currentTimeMillis();
+                    // Realiza otras acciones según tus necesidades
+                } else {
+                    // Si no hay mensajes recibidos durante el tiempo de espera del poller
+                    if (System.currentTimeMillis() - lastMessageTime > TIMEOUT) {
+                        // Llama a la función de redireccion
+                        redireccion();
+                        // Actualiza el tiempo del último mensaje para evitar llamadas múltiples
+                        lastMessageTime = System.currentTimeMillis();
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void redireccion() {
+        System.out.println("Redirigiendo...");
+        // Agrega aquí la lógica de redirección que necesitas
     }
 }
